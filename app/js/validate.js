2b06bc46eb6bd1061a17d31461fff07cc0cc0729
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2021-09-13 14:55:41 +0630

    feat: add validation for email sign up

---
+++ app/js/validate.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#signUpForm");
+const email = document.querySelector("#email");
+const emailAlert = document.querySelector("#emailAlert");
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+form.addEventListener('submit', function (event) {
+  if (!email.value || !email.value.match(mailformat)) {
+    emailAlert.style.display = "block";
+    event.preventDefault();
+  }
+});
+// https://www.w3resource.com/javascript/form/email-validation.php
